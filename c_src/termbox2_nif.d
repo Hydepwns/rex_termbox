e7e9d3b25be45906020413284dee0dc2e12eb15c
DROO <andrewtehsailor@gmail.com>
2025-05-27 18:37:04 +0200

    chore(nif): move termbox2_nif sources into c_src for project ownership and version control

---
+++ c_src/termbox2_nif.d
@@ -0,0 +1,5 @@
+c_src/termbox2_nif.o: c_src/termbox2_nif.c c_src/termbox2/termbox2.h \
+  /Users/droo/.asdf/installs/erlang/26.2.5/erts-14.2.5/include/erl_nif.h \
+  /Users/droo/.asdf/installs/erlang/26.2.5/erts-14.2.5/include/erl_drv_nif.h \
+  /Users/droo/.asdf/installs/erlang/26.2.5/erts-14.2.5/include/erl_int_sizes_config.h \
+  /Users/droo/.asdf/installs/erlang/26.2.5/erts-14.2.5/include/erl_nif_api_funcs.h
